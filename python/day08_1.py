3364e7a0fbacceaf90347b9eca63558c45e29336
Scott Halgrim <shalgrim@gmail.com>
2020-12-07 21:12:53 -0800

    day 8 part 1 success

---
+++ python/day08_1.py
@@ -0,0 +1,27 @@
+from file_ops import readlines
+ACCUMULATOR = 0
+EXECUTED = set()
+
+
+def execute_instruction(instruction, value):
+    global ACCUMULATOR
+    if instruction == 'jmp':
+        return int(value)
+    elif instruction == 'acc':
+        ACCUMULATOR += int(value)
+    return 1
+
+
+def run_until_second_execution(instructions):
+    global EXECUTED
+    instruction_pointer = 0
+
+    while instruction_pointer not in EXECUTED:
+        EXECUTED.add(instruction_pointer)
+        instruction_pointer += execute_instruction(*instructions[instruction_pointer])
+
+
+if __name__ == '__main__':
+    lines = readlines(8)
+    run_until_second_execution([line.split() for line in lines])
+    print(ACCUMULATOR)
